1893e0a5fea52535334216701847e627575369a4
Neo3691102 <nestor8772@gmail.com>
2025-10-31 18:08:21 -0600

    Actualizacion de codigo

--- blog/primersitioweb/script.js
+++ blog/primersitioweb/script.js
@@ -1,13 +1,26 @@
+//articulos
+const articuloH = document.getElementById("herramientas");
+const articuloI = document.getElementById("introduccion");
+
 const botonNext = document.getElementById("next");
 const botonLeft = document.getElementById("left");
 
+const cambiarArticulo = (idMostrar) => {
+  const articulos = document.querySelectorAll("article");
+
+  articulos.forEach(articulo => {
+    if (articulo.id === idMostrar) {
+      articulo.style.display = "flex";
+      articulo.style.flexDirection = "column";
+      articulo.style.alignItems = "center";
+      articulo.style.justifyContent = "center";
+    } else {
+      articulo.style.display = "none";
+    }
+  });
+}
+
+
 botonNext.addEventListener("click", () => {
-    console.log("boton next!!!");
-    const articuloH = document.getElementById("herramientas");
-    const articuloI = document.getElementById("introduccion");
-    articuloI.style.display = "none";
-    articuloH.style.display = "flex";
-    articuloH.style.flexDuirection = "column";
-    articuloH.style.alignItems = "center";
-    articuloH.style.justifyContent = "center";
-})
+  cambiarArticulo("herramientas");
+});
